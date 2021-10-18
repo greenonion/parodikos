20d6e90ef05dee6b6b045f71cf0e121b2d0eaf58
Nikos Fertakis <nikos@skroutz.gr>
2021-10-18 21:30:59 +0300

    Conform to rubocop

--- lib/parodikos.rb
+++ lib/parodikos.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-require_relative "parodikos/version"
-require_relative "parodikos/headers"
-require_relative "parodikos/signer"
-require_relative "parodikos/client"
-require_relative "parodikos/statuses"
+require_relative 'parodikos/version'
+require_relative 'parodikos/headers'
+require_relative 'parodikos/signer'
+require_relative 'parodikos/client'
+require_relative 'parodikos/statuses'
+require_relative 'parodikos/reaper'
 
 module Parodikos
   class Error < StandardError; end
