eb666c0d4ac2a56a03833384f02666616b7d6ae6
Nikos Fertakis <nikos@skroutz.gr>
2021-11-06 19:32:24 +0200

    Implement Reaper.dry_run
    This method finds and counts all tweets that would be deleted in a normal run

--- lib/parodikos/reaper.rb
+++ lib/parodikos/reaper.rb
@@ -1,63 +1,38 @@
 # frozen_string_literal: true
 
-require 'pry'
-
 module Parodikos
+  # Deletes all tweets before a certain date for a certain user.
+  # Allows for dry runs.
   class Reaper
-    MAX_COUNT = 200
+    attr_reader :screen_name, :before, :status_finder
 
-    attr_reader :screen_name, :before, :dry
-
-    def initialize(screen_name:, before:, dry: true)
+    def initialize(screen_name:, before:, status_finder: StatusFinder)
       @screen_name = screen_name
       @before = before
-      @dry = dry
+      @status_finder = status_finder
     end
 
-    def reap!
-      min_id
+    def dry!
+      StatusFinder.number_of_tweets_before(screen_name: screen_name, date: before)
+    end
+
+    def self.dry_run(screen_name:, before:)
+      new(screen_name: screen_name, before: before).dry!
     end
 
     def self.reap!(screen_name:, before:, dry: true)
-      new(screen_name: screen_name, before: before, dry: dry).reap!
+      if dry
+        dry_run(screen_name: screen_name, before: before)
+      else
+        false
+      end
     end
 
     private
 
-    # Finds the minimum tweet id that should not be deleted.
-    # Three options:
-    # - We have tweets both before and after the requested date, which means
-    #   we can find the min_id to start deleting
-    # - All tweets are newer so we have to look further back
-    # - There aren't any tweets before the requested date
-    def min_id
-      id = nil
-      max_id = nil
-
-      # Let's not really do an infinite loop here, 20k tweets should cover it
-      100.times do
-        tweets = tweets_before(max_id)
-        return nil if tweets.empty?
-
-        id = oldest_after(tweets)
-        return id if id
-
-        max_id = tweets.last['id']
-      end
-    end
-
-    def tweets_before(max_id)
-      Statuses.fetch(screen_name: screen_name, count: MAX_COUNT, max_id: max_id)
-              .sort_by { |status| DateTime.parse(status['created_at']) }
-              .reverse
-    end
-
-    def oldest_after(tweets)
-      oldest = tweets.select do |tweet|
-        before > DateTime.parse(tweet['created_at'])
-      end.last
-
-      oldest.nil? ? nil : oldest['id']
+    # The maximum id that should be deleted
+    def max_id
+      @max_id ||= StatusFinder.max_before(screen_name: screen_name, date: before)
     end
   end
 end
