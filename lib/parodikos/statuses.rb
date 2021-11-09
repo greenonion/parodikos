db64b7a83622cc549fb12f5727e602c3e6f8745c
Nikos Fertakis <nikos@skroutz.gr>
2021-11-09 08:32:18 +0200

    Fix Rubocop offences on Statuses class

--- lib/parodikos/statuses.rb
+++ lib/parodikos/statuses.rb
@@ -5,18 +5,13 @@ require 'json'
 module Parodikos
   # Resposible for interacting with the GET statues endpoint
   class Statuses
-    attr_reader :method, :url, :params, :client
+    attr_reader :method, :url, :client, :screen_name, :count, :since_id, :max_id
 
     def initialize(screen_name:, count:, since_id: nil, max_id: nil, client: Client)
-      @params = {
-        'screen_name' => screen_name,
-        'count' => count,
-        'exclude_replies' => true,
-        'trim_user' => true,
-        'since_id' => since_id,
-        'max_id' => max_id,
-        'include_rts' => true
-      }.compact
+      @screen_name = screen_name
+      @count = count
+      @since_id = since_id
+      @max_id = max_id
       @method = 'get'
       @url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
       @client = client
@@ -45,6 +40,18 @@ module Parodikos
 
     def ok?
       response.status == 200
+    end
+
+    def params
+      @params ||= {
+        'screen_name' => screen_name,
+        'count' => count,
+        'exclude_replies' => true,
+        'trim_user' => true,
+        'since_id' => since_id,
+        'max_id' => max_id,
+        'include_rts' => true
+      }.compact
     end
   end
 end
